70a8ae9957e994b4cc5097c0df7a5d776c0f7216
Michael Chierici <michael.chierici@deway.com.br>
2023-08-07 22:01:25 -0300

    fix title

--- src/constants/aboutMe.ts
+++ src/constants/aboutMe.ts
@@ -3,7 +3,7 @@ import { AboutMeProps } from "../types/AboutMe";
 export const AboutMe: AboutMeProps[] = [
   { title: "MICHAEL CHIERICI,", type: "name" },
   { title: "DESENVOLVEDOR", type: "profession" },
-  { title: "FULL STACK", type: "role" },
+  { title: "FULLSTACK", type: "role" },
   {
     description: `Formado em análise e desenvolvimento de sistemas, trabalho com tecnologia há mais de 2 anos, atualmente atuo como desenvolvedor Fullstack. Possuo grande capacidade em construir interfaces do absoluto zero, resolver problemas do dia a dia entregando demandas dentro do prazo.
      `,
